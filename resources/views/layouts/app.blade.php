94e20d05ccb7f08f7e645df1b2e23b142f13de85
BagjaNr19 <2306075@itg.ac.id>
2026-01-10 01:45:07 +0700

    force secure asset loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Sistem Resep Makanan')</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <<link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     @stack('styles')
 </head>
